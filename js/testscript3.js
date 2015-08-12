9abc7484a708e888270120e37b91294a7df6f62f
georgecode <georgeczurlanis@gmail.com>
2015-08-12 19:20:32 -0400

    key selection builder is now a function and can be used for multible objecty arrarys

--- js/testscript3.js
+++ js/testscript3.js
@@ -41,29 +41,38 @@ $(document).ready(function(){
 		})
 	}
 
-/*
+
 	var keys = {
-		eMinor: ['E3', 'G3', 'A3', 'B3', 'D4','E4','G4','A4','B4'],
-		XXXXXX: ['', '', '', '', '', '', '', '', '']
+		
+		eMinor: ['E3', 'G3', 'A3', 'B3', 'D4','E4','G4','A4','B4']
+
 	}
+	console.log(keys.eMinor[2]) 
+
+
+
+/*
+work(allowed,num,keyid,tone)
 */
+var keySelection =function(setOfNotes){
 
+	console.log(setOfNotes)
+	var n = setOfNotes
 
+//work('65a', 65, '.key1', 'E3')
 
+work('65a', 65, '.key1', n[0])
+work('83s', 83, '.key2', n[1])
+work('68d', 68, '.key3', n[2])
+work('70f', 70, '.key4', n[3])
+work('71g', 71, '.key5', n[4])
+work('72h', 72, '.key6', n[5])
+work('74j', 74, '.key7', n[6])
+work('75k', 75, '.key8', n[7])
+work('76l', 76, '.key9', n[8])
+}
 
-
-work('65a', 65, '.key1', 'E3')
-work('83s', 83, '.key2', 'G3')
-work('68d', 68, '.key3', 'A3')
-work('70f', 70, '.key4', 'B3')
-work('71g', 71, '.key5', 'D4')
-work('72h', 72, '.key6', 'E4')
-work('74j', 74, '.key7', 'G4')
-work('75k', 75, '.key8', 'A4')
-work('76l', 76, '.key9', 'B4')
-
-
-
+keySelection(keys.eMinor)
 
 //work('72h',72, '.key9', 'C4')
 
