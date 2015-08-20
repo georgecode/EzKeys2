70be1f43fd524d5693b059cf4cd058f14ce421de
georgecode <georgeczurlanis@gmail.com>
2015-08-20 03:58:33 -0400

    Added audio tags in html instead of creating them with scripts and fired them with html audio methodsinstead of useing audio autoplay... I hoped doing it this way would get rid of the left over speaker pop...it didn't :(

--- js/testscript3.js
+++ js/testscript3.js
@@ -3,74 +3,6 @@ $(document).ready(function(){
 //document.getElementById('B4').play()
 //$('#B4').animate({volume: 0.1}, 500)
 
-//2.This function constructs the mp3 HTML5 audio tags and content
-	var sound = function(note){
-	 //return '<audio id=' +note+ '> <source src="sounds/notes/' + note + '.mp3" type="audio/mpeg"></audio>'
- var x = 'sounds/notes/' + note + '.mp3'
- var z = note
-	var audioNote = document.createElement('audio');
-    audioNote.setAttribute('src', x)//; 'type'="audio/mpeg"; 'id', "'"note"'";);
-	audioNote.setAttribute('id', z)
-	console.log(audioNote)
-
-
-
-	}
-	
-	//console.log (sound('C4'))//Use this for Debugging
-//2.End----------------------
-
-//C Cx D Dx E F Fx G Gx A Ax B
-
-	var C3 = sound('C3')
-	var Cx3 = sound('Cx3')
-	var D3 = sound('D3')
-	var Dx3 = sound('Dx3')
-	var E3 = sound('E3')
-	var F3 = sound('F3')
-	var Fx3 = sound('Fx3')
-	var G3 = sound('G3')
-	var Gx3 = sound('Gx3')
-	var A3 = sound('A3')
-	var Ax3 = sound('Ax3')
-	var B3 = sound('B3')
-
-	var C4 = sound('C4')
-	var Cx4 = sound('Cx4')
-	var D4 = sound('D4')
-	var Dx4 = sound('Dx4')
-	var E4 = sound('E4')
-	var F4 = sound('F4')
-	var Fx4 = sound('Fx4')
-	var G4 = sound('G4')
-	var Gx4 = sound('Gx4')
-	var A4 = sound('A4')
-	var Ax4 = sound('Ax4')
-	var B4 = sound('B4')
-	
-
-
-//$( ".AllMp3s" ).append(C3, Cx3, D3, Dx3, E3, F3, Fx3, G3, Gx3, A3, Ax3, B3, C4, Cx4, D4, Dx4, E4, F4, Fx4, G4, Gx4, A4, Ax4, B4);
-
-//1. This object contains the arrays of sound clips for each key
-//	musical key (The key of whatever)
-	var keys = {
-		em: ['E3', 'G3', 'A3', 'B3', 'D4', 'E4', 'G4', 'A4', 'B4'],
-		//em: [E3, G3, A3, B3, D4, E4, G4, A4, B4],
-		fm: ['Dx3','F3', 'Gx3', 'Ax3','C4','Dx4','F4','Gx4','Ax4'],
-		fxm: ['E3', 'Fx3','A3', 'B3','Cx4','E4','Fx4','A4', 'B4'],
-		gm: ['D3','F3','G3','Ax3','C4','D4','F4','G4','Ax4'],
-		gxm: ['Dx3','Fx3','Gx3','B3','Cx4','Dx4','Fx4','Gx4','B4'],
-		am: ['C3','D3','E3','G3','A3','C4','D4','E4','G4'],
-		axm: ['Cx3','Dx3','F3','Gx3','Ax3','Cx4','Dx4','F4','Gx4'],
-		bm: ['D3','E3','Fx3','A3','B3','D4','E4','Fx4','A4'],
-		cm: ['C3','Dx3','F3','G3','Ax3','C4','Dx4','F4','G4'],
-		cxm: ['Cx3','E3','Fx3','Gx3','B3','Cx4','E4','Fx4','Gx4'],
-		dm: ['D3','F3','G3','A3','C4','D4','F4','G4','A4'],
-		dxm: ['Dx3','Fx3','Gx3','Ax3','Cx4','Dx4','Fx4','Gx4','Ax4']
-	}
-	//console.log(keys.em[2]) 
-//1.End----------------------
 
 
 
@@ -82,7 +14,7 @@ $(document).ready(function(){
                  //work('65a', 65, '.key1', n[0], 'A')
 
 	var work = function(allowed,num,keyid,tone,letter){
-
+			console.log("FUCK YOU")
 		var allowed = true;
 
 		$(document).on("keydown", function(xevent){
@@ -96,8 +28,20 @@ $(document).ready(function(){
 		        //$(keyid).html(letter + tone)
 		          //audioId = tone
 		          //console.log(audioId)
-		          console.log(tone)		       		         
-		          document.getElementById(tone).play();
+		          console.log(tone)	
+
+		          var xxx = '#'+ tone
+		        	console.log(xxx)
+
+		        	//$(xxx).animate({volume: 1.0});
+
+
+		        	$(xxx).stop(true);
+
+		        	var soundIdEl = document.getElementById(tone);
+					soundIdEl.currentTime=0;	
+		          soundIdEl.volume=1.0;
+		          soundIdEl.play();
 
 		        console.log(xevent.which)
 
@@ -106,12 +50,32 @@ $(document).ready(function(){
 
 		            if(xevent.which === num){            
 		            //$(keyid).html(letter)//Use for Testing		           
-		            audioId = "#" + tone
-		            var damn =audioId.toString()	
-		            console.log(damn)
+
+		            //var getIt = document.getElementById(tone);
+		            //getIt.addEventListener("ended",function(){
+		            //	this.currentTime = 0;
+		            //	this.pause();
+		            //})
+
+		        	var xxx = '#'+ tone
+		        	console.log(xxx)
+
+		        	$(xxx).animate({volume: 0.1}, 500)//, function(){
+		        		//document.getElementById(tone).pause();
+		        		
+
+		        	//});
+
+		            //document.getElementById(tone).pause();
+		            //document.getElementById(tone).currentTime=0;
+		            
+
+
+
+
 		            //document.getElementById(tone).pause()
 		            //document.getElementById(tone).animate({volume: 0.1}, 500)	       		         
-		         	$('#B4').animate({volume: 0.1}, 500);
+		         	//$('#B4').animate({volume: 0.1}, 500);
 		            $(keyid).removeClass('keyIsDown')
 
 		            allowed = true;
@@ -152,27 +116,26 @@ $(document).ready(function(){
 
 //4. This creates a set of notes and assigns keys 
 //  for each musical key
-	var keySelection =function(setOfNotes){
+	//var keySelection =function(setOfNotes){
 
 		//console.log(setOfNotes)//use this for debugging musically wrong notes
-		var n = setOfNotes
+		//var n = setOfNotes
 
 	//work('65a', 65, '.key1', 'E3')
 
-	work('65a', 65, '.key1', n[0], 'A')
-	work('83s', 83, '.key2', n[1], 'S')
-	work('68d', 68, '.key3', n[2], 'D')
-	work('70f', 70, '.key4', n[3], 'F')
-	work('71g', 71, '.key5', n[4], 'G')
-	work('72h', 72, '.key6', n[5], 'H')
-	work('74j', 74, '.key7', n[6], 'J')
-	work('75k', 75, '.key8', n[7], 'K')
-	work('76l', 76, '.key9', n[8], 'L')
-	}
+	work('65a', 65, '.key1', 'Dx4', 'A')
+	work('83s', 83, '.key2', 'E4', 'S')
+	work('68d', 68, '.key3', 'F4', 'D')
+	work('70f', 70, '.key4', 'Fx4', 'F')
+	work('71g', 71, '.key5', 'G4', 'G')
+	work('72h', 72, '.key6', 'Gx4', 'H')
+	work('74j', 74, '.key7', 'A4', 'J')
+	work('75k', 75, '.key8', 'Ax4', 'K')
+	work('76l', 76, '.key9', 'B4', 'L')
+	//}
 //4.End---------------------
 
-	
-keySelection(keys.em)
+})
 	
 
 //5.This is the menu used to make a musical key selection
@@ -182,24 +145,6 @@ keySelection(keys.em)
 	//})
 	
 
-var assignNotesToKey = function(ScaleKey){
-	$(ScaleKey).on("click", function(){
-			keySelection(keys[$(this).attr('class')])
-	})		   
-}
- assignNotesToKey(".em")
- assignNotesToKey(".fm")
- assignNotesToKey(".fxm")
- assignNotesToKey(".gm")
- assignNotesToKey(".gxm")
- assignNotesToKey(".am")
- assignNotesToKey(".axm")
- assignNotesToKey(".bm")
- assignNotesToKey(".cm")
- assignNotesToKey(".cxm")
- assignNotesToKey(".dm")
- assignNotesToKey(".dxm")
 
-//5.End--------------------
 
-})//----------end of document------
+//----------end of document------
